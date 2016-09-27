038de62c4af3293e9acefaeace2b986275b8233c
zvoanhkietz <zvoanhkietz@gmail.com>
2016-09-27 09:56:37 +0700

    Update bootstrap.php

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
  if (php_sapi_name() == "cli") {
-    echo 'Current PHP version: ' . phpversion() . '\n';
+    echo 'Current PHP version: ' . phpversion() . PHP_EOL;
  }
 require realpath(__DIR__. '/..') . '/vendor/autoload.php';
